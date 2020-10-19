d87534d68991504b156d943e4864496d2a502dfa
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-10-19 11:24:55 -0700

    Share channels.db between test cases.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- tests/test_fasm2bels.py
+++ tests/test_fasm2bels.py
@@ -19,6 +19,16 @@ pin_constraint_types = [PinConstraintType.XDC, PinConstraintType.PCF]
 
 
 class TestFasm2Bels(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.channels_file = tempfile.NamedTemporaryFile(suffix='channels.db', delete=False)
+        cls.channels_file.close()
+        os.unlink(cls.channels_file.name)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.unlink(cls.channels_file.name)
+
     @parameterized.expand(itertools.product(test_names, pin_constraint_types))
     def test_simple_ff(self, test_name, pin_constraint_type):
         cur_dir = os.path.dirname(__file__)
@@ -41,7 +51,6 @@ class TestFasm2Bels(unittest.TestCase):
             dir='/tmp')
 
         fasm_file = os.path.join(temp_dir, '{}.fasm'.format(test_name))
-        channels_file = os.path.join(temp_dir, 'channels.db')
 
         iostandard = 'LVCMOS33'
         drive = '12'
@@ -55,7 +64,7 @@ class TestFasm2Bels(unittest.TestCase):
             'fasm2bels', '--db_root', db_root, '--part', part, '--bitread',
             bitread, '--bit_file', bit_file, '--fasm_file', fasm_file,
             '--eblif', eblif, '--top', top, '--iostandard', iostandard,
-            '--drive', drive, '--connection_database', channels_file,
+            '--drive', drive, '--connection_database', self.channels_file.name,
             '--verilog_file', generated_top_v, '--xdc_file', generated_top_xdc
         ]
         if pin_constraint_type == PinConstraintType.XDC:
