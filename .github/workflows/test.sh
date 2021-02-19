a5804498c929bc99e308072eb3f80948822113f9
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-02-18 17:55:50 -0800

    Update FPGA interchange schema path.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export CAPNP_PATH="$GITHUB_WORKSPACE/capnproto-java/compiler/src/main/schema/"
-export INTERCHANGE_SCHEMA_PATH="$GITHUB_WORKSPACE/RapidWright/interchange"
+export INTERCHANGE_SCHEMA_PATH="$GITHUB_WORKSPACE/fpga-interchange-schema/interchange"
 
 make test-py
