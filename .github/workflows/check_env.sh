774157362a30d28fd5ac0bb82142489a345d1317
Jarom Harris <harrisjarom@gmail.com>
2021-10-07 12:31:58 -0600

    Added CI check for environment creation
    Signed-off-by: Jarom Harris <harrisjarom@gmail.com>

---
+++ .github/workflows/check_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright (C) 2020-2021  The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
+
+if [ ! -d "$(pwd)/env" ]
+then
+    echo "ERROR: make env failed"
+    return 1
+fi
